5cd6e497141fb1b5da780cd318599f7b5bb22f21
ks2526299 <kaylasmith526@gmail.com>
2015-01-06 21:20:40 -0800

    Signed-off-by: ks2526299 <kaylasmith526@gmail.com>

---
+++ ComputersMakeMistakes/main.cpp
@@ -0,0 +1,28 @@
+/* 
+ * File:   main.cpp
+ * Author: Kayla Smith
+ *
+ * Created on January 6, 2015, 9:03 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    //Declare Variables
+    float a,b,c;
+    //Prompt user for a and b
+    cout<<"input 2 short variable values"<<end1;
+    cin>>>a>>b;
+    //Calculate the sum
+    c=a+b;
+    //Output the results
+    cout<<"the sum c=a+b  -> "<<c<<end1;
+    
+    return 0;
+}
+
